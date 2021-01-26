13f518c34c1b16a292d54e63e5b39da9cb9068bc
Jeremy Hayes <jeremyh@liatrio.com>
2021-01-26 17:15:58 -0500

    use FQDN

--- tf/modules/nginx/outputs.tf
+++ tf/modules/nginx/outputs.tf
@@ -1,3 +1,3 @@
 output "service_url" {
-  value = "${helm_release.nginx.name}-controller.${kubernetes_namespace.nginx.metadata[0].name}"
+  value = "${helm_release.nginx.name}-controller.${kubernetes_namespace.nginx.metadata[0].name}.svc.cluster.local"
 }
