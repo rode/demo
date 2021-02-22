3a0439e143e3913c308d6aa18f41872c560abe22
Jasmine Francois <jfrancois@leantechniques.com>
2021-02-22 10:43:48 -0600

    Bump version of rode to 0.0.5

--- tf/modules/rode/main.tf
+++ tf/modules/rode/main.tf
@@ -9,7 +9,7 @@ resource "helm_release" "rode" {
   namespace  = kubernetes_namespace.rode.metadata[0].name
   chart      = "rode"
   repository = "https://rode.github.io/charts"
-  version    = "0.0.4"
+  version    = "0.0.5"
   wait       = true
 
   values = [
