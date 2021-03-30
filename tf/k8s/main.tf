d7fe9dba911051139b80076ab0c558ac63ef2b51
Alex Ashley <alexa@liatrio.com>
2021-03-30 13:03:53 -0500

    wip: bump provider versions

--- tf/k8s/main.tf
+++ tf/k8s/main.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "1.13.3"
+      version = "2.0.0"
     }
 
     helm = {
       source  = "hashicorp/helm"
-      version = "2.0.1"
+      version = "2.0.3"
     }
 
     random = {
