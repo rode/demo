d972f62620bf883dc6ba8b796d51f167264da55f
JonRudy <jonr@liatrio.com>
2021-04-12 14:16:14 -0700

    adding in new configuration for kubernetes

--- tf/k8s/environments/aws/main.tf
+++ tf/k8s/environments/aws/main.tf
@@ -23,9 +23,20 @@ terraform {
   }
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_caller_identity" "current" {}
+
 provider "kubernetes" {
-  config_context = var.kube_context
-  config_path    = "~/.kube/config"
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
+  }
 }
 
 provider "harbor" {
