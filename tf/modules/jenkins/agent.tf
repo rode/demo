1f49b2feb296d2c6e659b8eb1fe86f487931c59a
aalsabag <43807484+aalsabag@users.noreply.github.com>
2021-04-21 16:15:08 -0500

    Adding environment steps and removing depends on from prod deploy (#30)
    * update ui version to 5.0
    
    * added a separate sandbox and prod environment
    
    * changing host urls to match sanbox env
    
    * added job in github action to perform a sandbox deployment
    
    * changing the gitub runner for stage deployment
    
    * Added dev, staging, prod namespace for app deployment
    
    * remove sandbox deploy job
    
    * remove depends on step
    
    Co-authored-by: jknight-liatrio <joek@liatrio.com>

--- tf/modules/jenkins/agent.tf
+++ tf/modules/jenkins/agent.tf
@@ -20,10 +20,10 @@ resource "kubernetes_service_account" "jenkins_agent" {
 }
 
 // Add role permissions for Jenkins Agents
-resource "kubernetes_role" "jenkins_agent" {
+resource "kubernetes_role" "jenkins_agent_dev" {
   metadata {
     name      = "jenkins-agent"
-    namespace = var.deploy_namespace
+    namespace = "${var.deploy_namespace}-dev"
 
     labels = {
       "app.kubernetes.io/name"       = "jenkins"
@@ -63,10 +63,10 @@ resource "kubernetes_role" "jenkins_agent" {
 }
 
 // Bind Kubernetes secrets role to Jenkins service account
-resource "kubernetes_role_binding" "jenkins_agent" {
+resource "kubernetes_role_binding" "jenkins_agent_dev" {
   metadata {
     name      = "jenkins-agent"
-    namespace = var.deploy_namespace
+    namespace = "${var.deploy_namespace}-dev"
 
     labels = {
       "app.kubernetes.io/name"       = "jenkins"
@@ -84,7 +84,7 @@ resource "kubernetes_role_binding" "jenkins_agent" {
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "Role"
-    name      = kubernetes_role.jenkins_agent.metadata[0].name
+    name      = kubernetes_role.jenkins_agent_dev.metadata[0].name
   }
 
   subject {
@@ -94,3 +94,152 @@ resource "kubernetes_role_binding" "jenkins_agent" {
   }
 }
 
+// Add role permissions for Jenkins Agents
+resource "kubernetes_role" "jenkins_agent_staging" {
+  metadata {
+    name      = "jenkins-agent"
+    namespace = "${var.deploy_namespace}-staging"
+
+    labels = {
+      "app.kubernetes.io/name"       = "jenkins"
+      "app.kubernetes.io/instance"   = "jenkins"
+      "app.kubernetes.io/component"  = "jenkins-master"
+      "app.kubernetes.io/managed-by" = "Terraform"
+    }
+
+    annotations = {
+      description = "Permission required for Jenkins agents"
+      source-repo = "https://github.com/rode/demo"
+    }
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["services", "configmaps", "secrets", "namespaces", "serviceaccounts"]
+    verbs      = ["*"]
+  }
+
+  rule {
+    api_groups = ["apps"]
+    resources  = ["deployments", "statefulsets", "replicasets"]
+    verbs      = ["*"]
+  }
+  rule {
+    api_groups = ["networking.k8s.io"]
+    resources  = ["ingresses"]
+    verbs      = ["*"]
+  }
+  rule {
+    api_groups = ["policy"]
+    resources  = ["poddisruptionbudgets"]
+    verbs      = ["*"]
+  }
+
+}
+
+// Bind Kubernetes secrets role to Jenkins service account
+resource "kubernetes_role_binding" "jenkins_agent_staging" {
+  metadata {
+    name      = "jenkins-agent"
+    namespace = "${var.deploy_namespace}-staging"
+
+    labels = {
+      "app.kubernetes.io/name"       = "jenkins"
+      "app.kubernetes.io/instance"   = "jenkins"
+      "app.kubernetes.io/component"  = "jenkins-master"
+      "app.kubernetes.io/managed-by" = "Terraform"
+    }
+
+    annotations = {
+      description = "Permission required for Jenkins Agents"
+      source-repo = "https://github.com/rode/demo"
+    }
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = kubernetes_role.jenkins_agent_staging.metadata[0].name
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.jenkins_agent.metadata[0].name
+    namespace = var.namespace
+  }
+}
+
+// Add role permissions for Jenkins Agents
+resource "kubernetes_role" "jenkins_agent_prod" {
+  metadata {
+    name      = "jenkins-agent"
+    namespace = "${var.deploy_namespace}-prod"
+
+    labels = {
+      "app.kubernetes.io/name"       = "jenkins"
+      "app.kubernetes.io/instance"   = "jenkins"
+      "app.kubernetes.io/component"  = "jenkins-master"
+      "app.kubernetes.io/managed-by" = "Terraform"
+    }
+
+    annotations = {
+      description = "Permission required for Jenkins agents"
+      source-repo = "https://github.com/rode/demo"
+    }
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["services", "configmaps", "secrets", "namespaces", "serviceaccounts"]
+    verbs      = ["*"]
+  }
+
+  rule {
+    api_groups = ["apps"]
+    resources  = ["deployments", "statefulsets", "replicasets"]
+    verbs      = ["*"]
+  }
+  rule {
+    api_groups = ["networking.k8s.io"]
+    resources  = ["ingresses"]
+    verbs      = ["*"]
+  }
+  rule {
+    api_groups = ["policy"]
+    resources  = ["poddisruptionbudgets"]
+    verbs      = ["*"]
+  }
+
+}
+
+// Bind Kubernetes secrets role to Jenkins service account
+resource "kubernetes_role_binding" "jenkins_agent_prod" {
+  metadata {
+    name      = "jenkins-agent"
+    namespace = "${var.deploy_namespace}-prod"
+
+    labels = {
+      "app.kubernetes.io/name"       = "jenkins"
+      "app.kubernetes.io/instance"   = "jenkins"
+      "app.kubernetes.io/component"  = "jenkins-master"
+      "app.kubernetes.io/managed-by" = "Terraform"
+    }
+
+    annotations = {
+      description = "Permission required for Jenkins Agents"
+      source-repo = "https://github.com/rode/demo"
+    }
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = kubernetes_role.jenkins_agent_prod.metadata[0].name
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.jenkins_agent.metadata[0].name
+    namespace = var.namespace
+  }
+}
