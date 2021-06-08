590830031cc53172438340cc109cc09ddc110129
Michael Parker <michael@parker.gg>
2021-06-08 15:47:13 -0500

    configure sonarqube project and quality gate

--- tf/modules/sonarqube-config/main.tf
+++ tf/modules/sonarqube-config/main.tf
@@ -7,3 +7,25 @@ resource "sonarqube_user_token" "admin_token" {
   name       = "token"
   login_name = "admin"
 }
+
+resource "sonarqube_project" "rode_demo_app" {
+  name       = "rode:demo-app"
+  project    = "rode:demo-app"
+  visibility = "public"
+}
+
+resource "sonarqube_qualitygate" "no_vulns" {
+  name = "No Code Vulnerabilities"
+}
+
+resource "sonarqube_qualitygate_condition" "no_vulns" {
+  gatename  = sonarqube_qualitygate.no_vulns.id
+  metric    = "vulnerabilities"
+  threshold = "0"
+  op        = "GT"
+}
+
+resource "sonarqube_qualitygate_project_association" "rode_no_vulns" {
+  gatename   = sonarqube_qualitygate.no_vulns.id
+  projectkey = sonarqube_project.rode_demo_app.project
+}
